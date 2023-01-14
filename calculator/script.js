ccb3f7e0aac8d2a572f4917f712e3a2765db9c6b
Saanvi <saanviahuja1@gmail.com>
2023-01-14 16:11:34 +0530

    next: try to make calculator behaviour

--- calculator/script.js
+++ calculator/script.js
@@ -1,16 +1,36 @@
 window.addEventListener("load", function(){
     let operatorClicked = false
-    let currentValue = 0;
-    document.querySelectorAll('.operator').addEventListener('click', function(){
-        operatorClicked = true;
-    })
-    document.querySelectorAll('.number').addEventListener('click', function(){
-        let display = document.querySelector('.display')
-        if (display.innerText!==0 && operatorClicked === false){
-            // simply puts the number in currentValue and adds digit to number
-            buttonValue = 2 // FIX LATER
-            display.innerText += buttonValue
-            currentValue = parseFloat(display.innerText)
+    let currentTotal = 0;
+
+    function operate(num1, num2, operator) {
+        if (operator==='add') {
+            return num1 + num2
         }
+        if (operator==='subtract') {
+            return num1 - num2
+        }
+        if (operator==='multiply') {
+            return num1 * num2
+        }
+        if (operator==='divide') {
+            return num1 / num2
+        }
+    }
+
+    operators = document.querySelectorAll('.operator')
+    operators.forEach(operator => {
+        operator.addEventListener('click', function(){
+            operatorClicked = true;
+            operation = this.id
+        })
+    })
+    numbers = document.querySelectorAll('.number')
+    numbers.forEach(number => {
+        number.addEventListener('click', function(){
+            let display = document.querySelector('.display')
+            displayNumber = parseFloat(display.innerText)
+            buttonValue = this.id 
+            // try to figure out calculator behaviour
+        })
     })
 })
