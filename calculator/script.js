b5ff90af4d36f239061f9c521a3dfe8fff035644
Saanvi <saanviahuja1@gmail.com>
2023-01-14 17:00:51 +0530

    get num1 num2

--- calculator/script.js
+++ calculator/script.js
@@ -1,6 +1,8 @@
 window.addEventListener("load", function(){
     let operatorClicked = false
-    let currentTotal = 0;
+    let operation = 'e'
+    let num1 = 0;
+    let num2 = 0
 
     function operate(num1, num2, operator) {
         if (operator==='add') {
@@ -20,17 +22,28 @@ window.addEventListener("load", function(){
     operators = document.querySelectorAll('.operator')
     operators.forEach(operator => {
         operator.addEventListener('click', function(){
-            operatorClicked = true;
-            operation = this.id
+            let display = document.querySelector('.display')
+            if (this.id==='add'){display.innerText += '+'}
+            if (this.id==='subtract'){display.innerText += '-'}
+            if (this.id==='multiply'){display.innerText += '×'}
+            if (this.id==='divide'){display.innerText += '÷'}
+            operatorClicked = true
         })
     })
     numbers = document.querySelectorAll('.number')
     numbers.forEach(number => {
         number.addEventListener('click', function(){
             let display = document.querySelector('.display')
-            displayNumber = parseFloat(display.innerText)
             buttonValue = this.id 
             // try to figure out calculator behaviour
+            // to do: put everything in display, use split and then operate when user presses another op or equals to.
+            if (operatorClicked===false){
+                display.innerText += buttonValue
+                num1 = parseFloat(display.innerText)
+            }
+            if (operatorClicked===true){
+                num2 = display.innerText.split(/[+-×÷]/)[1]
+            }
         })
     })
 })
