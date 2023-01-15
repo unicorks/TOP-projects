0d7c35d9722b7bc7f32f4297e223ed3feaa8a609
Saanvi <saanviahuja1@gmail.com>
2023-01-15 18:45:26 +0530

    done, fix floats later

--- calculator/script.js
+++ calculator/script.js
@@ -5,24 +5,32 @@ window.addEventListener("load", function(){
     let num2 = 0
 
     function operate(num1, num2, operator) {
-        if (operator==='add') {
+        if (operator==='+') {
             return num1 + num2
         }
-        if (operator==='subtract') {
+        if (operator==='-') {
             return num1 - num2
         }
-        if (operator==='multiply') {
+        if (operator==='×') {
             return num1 * num2
         }
-        if (operator==='divide') {
+        if (operator==='÷') {
             return num1 / num2
         }
     }
-
+    
+    // HOW IT WORKS: put everything in display, use split and then operate when user presses another op or equals to.
     operators = document.querySelectorAll('.operator')
     operators.forEach(operator => {
         operator.addEventListener('click', function(){
             let display = document.querySelector('.display')
+            if (operatorClicked === true) {
+                let temp = display.innerText.search(/[^0-9]/)
+                operation = display.innerText[temp]
+                num1 = operate(num1, num2, operation)
+                display.innerText = num1
+                operatorClicked = false
+            }
             if (this.id==='add'){display.innerText += '+'}
             if (this.id==='subtract'){display.innerText += '-'}
             if (this.id==='multiply'){display.innerText += '×'}
@@ -35,15 +43,31 @@ window.addEventListener("load", function(){
         number.addEventListener('click', function(){
             let display = document.querySelector('.display')
             buttonValue = this.id 
-            // try to figure out calculator behaviour
-            // to do: put everything in display, use split and then operate when user presses another op or equals to.
             if (operatorClicked===false){
                 display.innerText += buttonValue
                 num1 = parseFloat(display.innerText)
             }
             if (operatorClicked===true){
-                num2 = display.innerText.split(/[+-×÷]/)[1]
+                display.innerText += buttonValue
+                num2 = parseFloat(display.innerText.split(/[^0-9]/)[1])
             }
         })
+    })
+
+    document.querySelector('.clear').addEventListener('click', function(){
+        document.querySelector('.display').innerText = 0
+        num1 = 0
+        num2 = 0
+    })
+    document.querySelector('.equals').addEventListener('click', function(){
+        if (operatorClicked===false) {
+            return
+        }
+        let display = document.querySelector('.display')
+        let temp = display.innerText.search(/[^0-9]/)
+        operation = display.innerText[temp]
+        num1 = operate(num1, num2, operation)
+        display.innerText = num1
+        operatorClicked = false
     })
 })
