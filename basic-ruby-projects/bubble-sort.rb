ef2ebe5bc65912f696ca905de283403b27d7b857
Saanvi <saanviahuja1@gmail.com>
2023-01-27 16:45:28 +0530

    make bubble sort

---
+++ basic-ruby-projects/bubble-sort.rb
@@ -0,0 +1,24 @@
+def bubble_sort(array)
+    iterations = false
+    # runs sort till even a single iteration is taking place
+    while true
+        # runs sort once
+        for i in 0..array.size-2
+            if array[i] > array[i+1]
+                array.insert(i+1, array.delete_at(i))
+                iterations = true
+                next
+            end
+        end
+        # logic for checking whether array is sorted
+        if iterations == true
+            iterations = false
+            next        
+       else
+            break
+       end        
+    end
+    array
+end
+
+p bubble_sort([4,3,78,2,0,2])
