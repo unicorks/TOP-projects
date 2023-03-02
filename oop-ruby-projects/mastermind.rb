dd0514e35b1cec4adc95d30fae857174b9104c58
Saanvi <saanviahuja1@gmail.com>
2023-03-02 18:15:05 +0530

    make with user as codebreaker

--- oop-ruby-projects/mastermind.rb
+++ oop-ruby-projects/mastermind.rb
@@ -1,7 +1,8 @@
 # TODO:  MAKE MASTERMIND
 # MAKE CLASSES FOR CODEMAKER, CODEBREAKER, GAME
 
-def Game
+class Game
+  attr_accessor :codemaker, :codebreaker, :board
 
   def initialize(codemaker, codebreaker, board)
     @codemaker = codemaker
@@ -9,61 +10,74 @@ def Game
     @board = board
   end
 
-  def self.instructions
-    puts "Hello and welcome to Mastermind on the command line against the computer!
-    - Please enter your guess in the form '1234' where each digit of the guess is a number 1-8
-    - There are not supposed to be any duplicate digits in the code
-    - For the hints, 'p' will be shown for a perfect guess, 'e' will be shown for a guess which exists in the code
-    - There are 12 maximum turns to guess the code
-    - Guesses will be on the left and hints will be on the right of the game board
-
-    What would you like to be? Enter 0 for codemaker and 1 for codebreaker."
-  end
-
-  def start
-    # know whether user will be codemaker or codebreaker
+  def self.code_retriever
     while true
-      choice = gets.chomp
-      if choice  ==  "0" || choice  ==  "1"
+      code = gets.chomp.split('')
+      if code.length == 4 && (code.all? {|element| element.to_i >= 1 && element.to_i <=8 })
         break
       else
-        puts "Enter valid choice."
+        puts "Enter valid code."
       end
     end
+    code.map {|num| num.to_i}
+  end
 
-    # get name, initialise class instances
-    choice = choice.to_i
-    puts "Please enter your name: "
-    name_of_user = gets.chomp
-    if choice  ==  0
-      while true
-        code = gets.chomp.split('')
-        valid = true
-        for i in code
-          if i <= 8 && i >= 1
-            valid = false
-            break
-          end
-        end
-        if code.length > 4 || code.length < 4
-          valid = false
-        end
-        if valid = true
-          break
-        else
-          puts "Enter valid code."
-        end
+  def give_hints(code, guess)
+    hints = []
+    for i in 0..3
+      if code[i] == guess[i]
+        hints.append('p')
+      elsif code.include?(guess[i])
+        hints.append('e')
+      else
+        hints.append('○')
       end
-      codebreaker = Codebreaker.new('computer')
-      codemaker = Codemaker.new(name_of_user, code)
-    elsif choice  ==  1
-      codemaker = Codemaker.new('computer')
-      codebreaker = Codebreaker.new(name_of_user)
+    end
+    hints.shuffle
+  end
+
+  def self.instructions
+    puts "Hello and welcome to Mastermind on the command line against the computer!
+- Please enter your guess in the form '1234' where each digit of the guess is a number 1-8
+- There are NOT supposed to be any duplicate digits in the code
+- For the hints, 'p' will be shown for a perfect guess, 'e' will be shown for a guess which exists in the code
+- There are 12 maximum turns to guess the code
+- Guesses will be on the left and hints will be on the right of the game board
+
+What would you like to be? Enter 0 for codemaker and 1 for codebreaker."
+  end
+
+  def play
+    if codemaker.name == 'computer'
+      turns = 1
+      code = codemaker.code
+      while turns <= 12
+        puts "Turn #{turns}:"
+        board.print
+        puts "Enter your guess: "
+        guess = Game.code_retriever
+        if code.join == guess.join
+          puts "You win! Congrats."
+          exit
+        end
+        guesses = board.guesses
+        guesses[turns-1] = guess
+        board.guesses = guesses
+        hints = board.hints
+        hints[turns-1] = give_hints(code, guess)
+        board.hints = hints
+        turns += 1
+      end
+      puts "Oof, you lose. Better luck next time."
+      exit
+    else
+      puts "Sorry, playing as the codemaker isn't available yet. Check back later."
     end
   end
+
 end
 
-def Board
+class Board
   attr_accessor :guesses, :hints
 
   def initialize
@@ -74,27 +88,60 @@ def Board
   def print
     puts "╔═════════════╦═════════╗"
     for i in 0..11 do
-      puts "║ #{@guesses[i][]}  #{@guesses[i][]}  #{@guesses[i][]}  #{@guesses[i][]}  ║ #{@hints[i][]} #{@hints[i][]} #{@hints[i][]} #{@hints[i][]} ║
-      ╠═════════════╬═════════╣"
-    end
+      puts "║ #{@guesses[i][0]}  #{@guesses[i][1]}  #{@guesses[i][2]}  #{@guesses[i][3]}  ║ #{@hints[i][0]} #{@hints[i][1]} #{@hints[i][2]} #{@hints[i][3]} ║"
+      puts "╠═════════════╬═════════╣" unless i==11
     end
     puts "╚═════════════╩═════════╝"
   end
 end
 
-def Codemaker
+class Codemaker
   attr_reader :name, :code
 
-  def initialize(name, code=[Random.rand(1..8), Random.rand(1..8), Random.rand(1..8), Random.rand(1..8)].join.to_i)
+  def initialize(name, code=[Random.rand(1..8), Random.rand(1..8), Random.rand(1..8), Random.rand(1..8)])
     @name = name
-    @code = code.split('')    # verify the code if user is codemaker
+    @code = code
   end
 end
 
-def Codebreaker
+class Codebreaker
   attr_reader :name
 
   def initialize(name)
     @name = name
   end
 end
+
+def start
+  Game.instructions
+  # know whether user will be codemaker or codebreaker
+  while true
+    choice = gets.chomp
+    if choice == "0" || choice == "1"
+      break
+    else
+      puts "Enter valid choice."
+    end
+  end
+
+  # get name, initialise class instances
+  choice = choice.to_i
+  puts "Please enter your name: "
+  name_of_user = gets.chomp
+  if choice == 0
+    code = Game.code_retriever
+    codebreaker = Codebreaker.new('computer')
+    codemaker = Codemaker.new(name_of_user, code)
+  elsif choice == 1
+    codemaker = Codemaker.new('computer')
+    codebreaker = Codebreaker.new(name_of_user)
+  end
+
+  # initialise new game
+  game = Game.new(codemaker, codebreaker, Board.new)
+  game.play
+end
+
+
+start
+
