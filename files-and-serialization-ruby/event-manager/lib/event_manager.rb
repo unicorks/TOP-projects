20a8ee1e1e4e6f0cb6cf0dd2c3bba69c4d5d05c5
Saanvi <saanviahuja1@gmail.com>
2023-03-08 18:03:26 +0530

    follow tutorial

---
+++ files-and-serialization-ruby/event-manager/lib/event_manager.rb
@@ -0,0 +1,24 @@
+require 'csv'
+
+puts 'EventManager initialized.'
+
+contents = CSV.open(
+  'event_attendees.csv',
+  headers: true,
+  header_converters: :symbol
+)
+
+contents.each do |row|
+  name = row[:first_name]
+  zipcode = row[:zipcode]
+
+  if zipcode.nil?
+    zipcode = '00000'
+  elsif zipcode.length < 5
+    zipcode = zipcode.rjust(5, '0')
+  elsif zipcode.length > 5
+    zipcode = zipcode[0..4]
+  end
+
+  puts "#{name} #{zipcode}"
+end
