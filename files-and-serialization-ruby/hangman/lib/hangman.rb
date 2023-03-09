ca9e469966c8a44f86305661b60896b0b97b999a
Saanvi <saanviahuja1@gmail.com>
2023-03-09 17:47:36 +0530

    basic structure of player class

--- files-and-serialization-ruby/hangman/lib/hangman.rb
+++ files-and-serialization-ruby/hangman/lib/hangman.rb
@@ -1,1 +1,23 @@
-# TODO: choose whether continued game or new, load dictionary, choose word, show hint to user, track correct + incorrect letters, serialise when user wants to save game. need hangman or player class
+# TODO: choose whether continued game or new, show hint to user, track correct + incorrect letters, serialise when user wants to save game. need hangman or player class
+
+class Player
+  attr_accessor :guessed_letters, :guessed_word, :wrong_guesses_left
+  attr_reader :word
+
+  def initialise
+    @guessed_letters = {}
+    @word = pick_word
+    @guessed_word = Array.new(@word.length, '_')
+    @wrong_guesses_left = 12
+  end
+
+  private
+
+  def pick_word
+    File.open('dictionary.txt', 'r', &:readlines).filter{ |e| e.length >= 5 && e.length <= 12 }.sample
+  end
+
+  def get_letter
+    # get letter from user
+  end
+end
