51237c61b9a62a9c2d571302cfe4419d83a5b4ca
unicorks <81965694+unicorks@users.noreply.github.com>
2023-03-15 07:24:22 +0000

    practice recursion

---
+++ recursion-ruby/practice.rb
@@ -0,0 +1,15 @@
+# sum of multiples of 3 or 5 under 1000, from project euler
+
+def sum_3_5(n)
+    if n == 3
+        return 3
+    elsif n == 5
+        return 5
+    elsif n % 3 == 0 || n % 5 == 0
+        return n + sum_3_5(n-1)
+    else
+        return sum_3_5(n-1)
+    end
+end
+
+p sum_3_5(999)
