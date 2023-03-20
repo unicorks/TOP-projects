7d13cfc2cc78de8cb7108ed3b1f897dc25dd0642
Saanvi <saanviahuja1@gmail.com>
2023-03-20 17:14:02 +0530

    bst (traversals, balancing) (completed)

--- dsa-ruby-projects/binary_search_trees.rb
+++ dsa-ruby-projects/binary_search_trees.rb
@@ -79,6 +79,69 @@ class Tree
     end
   end
 
+  def level_order(queue = [@root], arr = [])
+    return if queue.empty?
+
+    arr << queue[0].data
+    queue.push(queue[0].left) unless queue[0].left.nil?
+    queue.push(queue[0].right) unless queue[0].right.nil?
+    queue.shift
+    level_order(queue, arr)
+    arr
+  end
+
+  def pre_order(root = @root, arr = [])
+    return if root.nil?
+
+    arr << root.data
+    pre_order(root.left, arr)
+    pre_order(root.right, arr)
+    arr
+  end
+
+  def in_order(root = @root, arr = [])
+    return if root.nil?
+
+    in_order(root.left, arr)
+    arr << root.data
+    in_order(root.right, arr)
+    arr
+  end
+
+  def post_order(root = @root, arr = [])
+    return if root.nil?
+
+    post_order(root.left, arr)
+    post_order(root.right, arr)
+    arr << root.data
+    arr
+  end
+
+  def height(root = @root)
+    return 0 if root.nil?
+
+    left_height = height(root.left)
+    right_height = height(root.right)
+    left_height > right_height ? left_height + 1 : right_height + 1
+  end
+
+  def depth(data)
+    height(@root) - height(find(data))
+  end
+
+  def balanced?(root = @root)
+    return if root.nil?
+    return false if (height(root.left) - height(root.right)).abs > 1
+
+    balanced?(root.left)
+    balanced?(root.right)
+    true
+  end
+
+  def rebalance(root = @root)
+    self.root = build_tree(in_order(root))
+  end
+
   def pretty_print(node = @root, prefix = '', is_left = true)
     pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
     puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
@@ -87,5 +150,18 @@ class Tree
 end
 
 # driver code
-e = Tree.new([1, 7, 4, 23, 8])
+e = Tree.new(Array.new(15) { rand(1..100) })
 e.pretty_print
+puts e.balanced?
+p "Level order: #{e.level_order}"
+p "Pre order: #{e.pre_order}"
+p "In order: #{e.in_order}"
+p "Post order: #{e.post_order}"
+5.times { e.insert(rand(100..200)) }
+puts e.balanced?
+e.rebalance
+puts e.balanced?
+p "Level order: #{e.level_order}"
+p "Pre order: #{e.pre_order}"
+p "In order: #{e.in_order}"
+p "Post order: #{e.post_order}"
