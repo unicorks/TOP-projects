e808260fbecc231cb028b291f600aba3e43c3c6e
Saanvi <81965694+unicorks@users.noreply.github.com>
2023-06-27 07:34:50 +0000

    begin making game class

--- chess-ruby/lib/chess_game.rb
+++ chess-ruby/lib/chess_game.rb
@@ -1,7 +1,30 @@
 require_relative 'chess_board'
 require_relative 'chess_player'
 
-board = Board.new
-tmp = board.board
-king = tmp[0][4]
-print king.valid_moves([4, 3])
+class Game
+    attr_accessor :player_1, :player_2, :board, :turn
+
+    def initialise
+        @player_1 = Player.new()
+        @player_2 = Player.new()
+        @board = Board.new()
+        @turn = player_1
+    end
+
+    def get_move
+        while true
+            puts "Enter your move, #{turn.name} {#{turn.color.upcase}}"
+            move = gets.chomp
+            # check if move is valid, todo
+            break if move_valid?
+        end
+    end
+
+    def move_valid?
+        # TODO
+    end
+
+    def switch_turn
+        self.turn = turn == player_1 ? player_2 : player_1
+    end
+end
